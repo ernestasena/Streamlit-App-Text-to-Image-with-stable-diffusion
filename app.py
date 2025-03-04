07273b44254dd740de02e66e89af99abcbf36961
ernestasena <ernestasena@gmail.com>
2025-03-04 23:53:13 +0300

    change prompt

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ model_id = "sd-legacy/stable-diffusion-v1-5"
 pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
 pipe = pipe.to("cuda")
 
-prompt = "a photo of an astronaut riding a red polar bear on mars"
+prompt = "a photo of an astronaut riding a red bear on mars"
 image = pipe(prompt).images[0]  
     
 image.save("astronaut_rides_horse.png")
